0f591a1e5ea193702263f4a0f2487d633dac4ded
minjongkim <rlaalswhd200@naver.com>
2021-12-20 21:03:43 +0900

    Initial Commit

---
+++ RisingCamp_Week5_CloneApp_MangoPlate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingCamp_Week5_CloneApp_MangoPlate
+//
+//  Created by 김민종 on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
